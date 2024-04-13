6bf788125a49cb897605767eee75d79689a4d20b
Paulina Udeh <udehpaulina93@gmail.com>
2024-04-13 14:36:26 +0000

    Sky is the limit, let's bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# This manifest is to use apachebench to stimate HTTP requests
+# To the web server.
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
